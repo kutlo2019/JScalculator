d6a4bc919f91ec8417a4459eea5a9cfd6197a7c1
kutlo2019 <kutlopopo@rocketmail.com>
2022-01-24 17:00:11 +0200

    add handle equal function, but only works for addition

--- src/App.js
+++ src/App.js
@@ -6,23 +6,43 @@ class App extends React.Component {
 
     this.state = {
       displayInput: '0',
-      answer: ''
+      answer: 'expr ='
     };
 
     this.handleClick = this.handleClick.bind(this);
     this.handleDelete = this.handleDelete.bind(this);
     this.clearScreen = this.clearScreen.bind(this);
+    this.handleEquals = this.handleEquals.bind(this);
+  }
+
+  handleEquals() {
+    this.setState( state => {
+        const string = state.displayInput;
+        let answer = string
+        if (string.charAt(0) === '0') {
+          answer = string.substring(1);
+        }
+        const answerString = answer.slice(0,-1)
+        const answerInt = eval(answerString)
+        console.log(answerString);
+        return {
+          displayInput: answerInt,
+          answer: answer
+        }
+      }
+    )
   }
 
   clearScreen() {
     this.setState({
-      displayInput: '0'
+      displayInput: '0',
+      answer: 'expr ='
     })
   }
 
   handleDelete() {
-      this.setState( state => {
-        const newDisplay = state.displayInput.slice(0,-1)
+    this.setState( state => {
+        const newDisplay = state.displayInput.slice(0,-1);
         return {
           displayInput: newDisplay
         }
@@ -44,13 +64,14 @@ class App extends React.Component {
       <div className="App">
         <h1 id="header">JavaScript Calculator</h1>
         <div className="display">
-          <div id="answer"></div>
+          <div id="answer">{this.state.answer}</div>
           <div id="display-button">{this.state.displayInput}</div>
         </div>
         <Buttons 
           handleClick={this.handleClick}
           handleDelete={this.handleDelete}
           clearScreen={this.clearScreen}
+          handleEquals={this.handleEquals}
         />
       </div>
     );
@@ -94,7 +115,7 @@ function Buttons(props) {
         <button className="btn" id="add">{'\u002B'}</button>
         <button className="btn" id="subtract">{'\u2212'}</button>
         <button className="number btn" id="zero">0</button>
-        <button className="btn" id="equals">=</button>
+        <button className="btn" id="equals" onClick={props.handleEquals}>=</button>
       </div>
       <div className="clean-up">
         <button onClick={props.handleDelete}>Del</button>
